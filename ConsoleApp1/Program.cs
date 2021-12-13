c9875287ac1e6f0fc8716a03e5e57b0dd802d215
[umid shamdinli] <[umidash@code.edu.az]>
2021-12-13 22:57:29 +0400

    Add operator overloading task

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person("Umid ","Shamdinli",21);
+
+            Person person1 = new Person("Resul", "Teymurlu", 20);
+
+            Console.WriteLine(person1.Age);
+            Console.WriteLine(person.Age);
+
+                
+                
+
+        }
+    }
+}
